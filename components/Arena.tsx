3b5815bc2f1dc69472573cb50ad54b3042754fb4
kai <kai@kodebyraaet.no>
2021-11-26 16:07:50 +0100

    arena

---
+++ components/Arena.tsx
@@ -0,0 +1,51 @@
+
+import { auth } from '~/../firebase';
+import { useEffect, useState } from 'react'
+
+function Arena({players, game}) {
+    const [loggedInUser, setLoggedInUser] = useState(null);
+    console.log('players', players);
+    console.log('game', game);
+
+    
+    const sortedPlayers = {}
+    for (const player of players) {
+        sortedPlayers[player.id] = player;
+    }
+    const player1 = {...game.players[0], ...sortedPlayers[game.players[0].id]}
+    const player2 = {...game.players[1], ...sortedPlayers[game.players[1].id]}
+
+    return (
+        <>
+            <div className="flex flex-row content-between p-20">
+                <Player player={player1}/>
+                <VS/>
+                <Player player={player2}/>
+            </div>
+            <p className="hidden">It is your turn</p>
+        </>
+    )
+}
+
+function Player({player}) {
+    
+    const imgSrc = player?.photoURL || "https://i.pravatar.cc/150?u=" + Math.random();
+    return(
+        <div className="flex flex-col flex-1">
+            <img src={imgSrc} className=""/>
+            <p className="text-center">
+                {player.displayName}
+            </p>
+        </div>
+    )
+}
+
+function VS() {
+    return (
+        <div className="flex-1  content-center">
+            <img className="justify-self-center" src="vs.jpeg"/>
+        </div>
+    )
+}
+
+export default Arena;
--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,13 @@
 import { useContext } from "react";
 import Auth from "~/components/Auth";
 import Card from "~/components/ui/Card";
+import Arena from "~/components/Arena"
 import Selector from "~/stories/Selector";
 import Actionables from "~/components/Actionables";
 import { TournamentContext } from "context";
 import useCurrentlyActiveUsers from "~/hooks/useCurrentlyActiveUsers";
 import TournamentVisual from '~/components/TournamentVisual';
+import useOngoingTournament from "~/hooks/useOngoingTournament";
 
 export default function Home() {
 	const { tournament, game, canChoose } = useContext(TournamentContext);
@@ -27,12 +29,19 @@ export default function Home() {
 				</>
 			}
 			{tournamentStarted &&
-				<div className='container'>
-					<TournamentVisual tournament={tournament} />
-					<Selector />
-				</div>
+        <>
+          <Auth />
+          <div className='container text-center'>
+            <Arena game={game} players={activeUsers} />
+            <Selector />
+
+            <TournamentVisual tournament={tournament} />
+        
+          </div>
+        </>
 			}
 			{!tournamentStarted && <Actionables />}
 		</section>
 	);
 }
+
